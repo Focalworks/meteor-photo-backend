d9559d92ed3f2b5f0e93d75a709b3de546450dab
Amitav Roy <reachme@amitavroy.com>
2015-02-23 13:14:18 +0530

    Basic authentication on routes added. Need to change the routes to use the controller

--- libs/routes.js
+++ libs/routes.js
@@ -1,56 +1,63 @@
 /**
  * Created by Amitav Roy on 21/2/15.
  */
+AdminController = RouteController.extend({
+  onBeforeAction: function () {
+    if (Meteor.user() === null) {
+      Router.go('/');
+    }
+
+    this.next();
+  }
+});
+
 Router.configure({
-    layoutTemplate: 'layout'
+  layoutTemplate: 'layout'
 });
 
 Router.route('/', function () {
-    this.render('registation');
+  this.render('home');
 });
 
-Router.route('/device-listing', function () {
-    this.render('registation');
+Router.route('/device-listing', {
+  controller: 'AdminController',
+  template: 'deviceDetails'
 });
 
 Router.route('/device-details/:_id', function () {
-    var device = DeviceRegistration.findOne({_id: this.params._id});
-    console.log(device);
-    this.render('deviceDetails', {data: device});
+  var device = DeviceRegistration.findOne({_id: this.params._id});
+  this.render('deviceDetails', {data: device});
 });
 
 HTTP.methods({
-    'hello': {
-        requestHeaders: function (request) {
-            console.log("Req" + request);
-        },
-        method: function(data) {
-            return EJSON.stringify({
-                text: "asadadsd",
-                data: ["1", "2"]
-            });
-        }
+  'hello': {
+    requestHeaders: function (request) {
+      console.log("Req" + request);
+    },
+    method: function(data) {
+      return EJSON.stringify({
+        text: "asadadsd",
+        data: ["1", "2"]
+      });
     }
+  }
 });
 
 if (Meteor.isServer) {
-    Router.onBeforeAction(Iron.Router.bodyParser.urlencoded({
-        extended: false
-    }));
+  Router.onBeforeAction(Iron.Router.bodyParser.urlencoded({
+    extended: false
+  }));
 }
 
-Router.route('/post', function () {
-    body = this.request.body;
+Router.route('/device-register', function () {
+  body = this.request.body;
 
-    var id = Meteor.call('registerDevice', body.mobileNumber, body.registrationId, body.deviceId);
-    var device = DeviceRegistration.find({"_id": id}).fetch();
-    var gcmRegId = device[0].regId;
+  var id = Meteor.call('registerDevice', body.mobileNumber, body.registrationId, body.deviceId);
+  var device = DeviceRegistration.find({"_id": id}).fetch();
+  var gcmRegId = device[0].regId;
 
-    /*var ids = [];
-    device.forEach(function (dev) {
-        ids.push(dev.regId);
-    });*/
-
-    Meteor.call('sendDeviceRegisteredNotification', gcmRegId);
-    this.response.end("Call served: " + id);
+  Meteor.call('sendDeviceRegisteredNotification', gcmRegId);
+  this.response.end("Call served: " + id);
 }, {where: 'server'});
+
+Router.route('testing')
