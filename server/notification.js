fec0748eee8b1aa0287e12d5ef3e2cbc4f400f84
Amitav Roy <reachme@amitavroy.com>
2015-03-03 13:43:06 +0530

    Notification with required data is now going as part of notification.

--- server/notification.js
+++ server/notification.js
@@ -12,14 +12,16 @@ Meteor.methods({
                 "registration_ids": [id],
                 "data": {
                     "title": "Congratulations",
-                    "message": "Your device is now registered with us."
+                    "message": "Your device is now registered with us.",
+                    "type": 'sendDeviceRegisteredNotification'
                 }
             }
         }, function (error, result) {
 
         });
     },
-    sendPhotoSetMessage: function (ids) {
+    sendPhotoSetMessage: function (ids, photoSet) {
+        console.log(photoSet[0]);
         HTTP.call("POST", "https://android.googleapis.com/gcm/send", {
             headers: {
                 'Content-Type': 'application/json',
@@ -28,8 +30,13 @@ Meteor.methods({
             data: {
                 "registration_ids": ids,
                 "data": {
-                    "title": "Photoset shared with you",
-                    "message": "You have a new photo set shared."
+                    "title": "Photo set " + photoSet[0].name + "shared with you",
+                    "message": "You have a new photo set shared.",
+                    "type": 'sendPhotoSetMessage',
+                    "photoSetTitle": photoSet[0].name,
+                    "createdByNumber": photoSet[0].createdBy,
+                    "urls": photoSet[0].urls,
+                    "created": photoSet[0].created
                 }
             }
         }, function (error, result) {
@@ -38,15 +45,16 @@ Meteor.methods({
     },
     sendNotificationToGroupAboutPhotoset: function (photoSetId) {
         photoSet = PhotoSet.find({_id: photoSetId}).fetch();
+
         distId = photoSet[0].distListId;
         distList = DistributionList.find({_id: distId}).fetch();
 
-        var deviceIds = [];
+        var regIds = [];
         _.each(distList[0].members, function (member) {
             var device = DeviceRegistration.find({mobileNumber: member}).fetch();
-            deviceIds.push(device[0].deviceId);
+            regIds.push(device[0].regId);
         });
 
-        Meteor.call('sendPhotoSetMessage', deviceIds);
+        Meteor.call('sendPhotoSetMessage', regIds, photoSet);
     }
 });
